1a05d11360fcdaab698af49c3174de7e2099f302
Diana <kkover073@gmail.com>
2025-09-25 18:48:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace laba5_Kuzmina_ISP_232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers;
+            int[]
+        }
+    }
+}
