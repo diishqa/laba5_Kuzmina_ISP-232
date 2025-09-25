1b41108713a80e05bea4556a99470d375292acfb
Diana <kkover073@gmail.com>
2025-09-25 21:41:49 +0300

    end

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace laba5_Kuzmina_ISP_232
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace laba5_Kuzmina_ISP_232
 {
     internal class Program
     {
@@ -139,23 +142,118 @@
             //{
             //    Console.WriteLine(person);
             //}
-            List<string> student = ["Билли", "Игорь", "Степан"];
-            student.Add("Van");
-            student.AddRange(["Антон", "Денис"]);
-            student.Insert(1, "Кирилл"); //вставляет элемент по индексу
-            student.InsertRange(1, ["Mike", "Kate"]);
-            sudent.Insert(7, "Михаил");
-            foreach(var person in student)
-            {
-                Console.WriteLine(person);
-            }
-            var people = new List<string>() { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam", "Tom", "Al" };
-            people.RemoveAt(1); //удаляется элемент по индексу
-            people.Remove("Tom");
-            people.RemoveRange(1, 2);
-            people.Clear();
+            //List<string> student = ["Билли", "Игорь", "Степан"];
+            //student.Add("Van");
+            //student.AddRange(["Антон", "Денис"]);
+            //student.Insert(1, "Кирилл"); //вставляет элемент по индексу
+            //student.InsertRange(1, ["Mike", "Kate"]);
+            //sudent.Insert(7, "Михаил");
+            //foreach(var person in student)
+            //{
+            //    Console.WriteLine(person);
+            //}
+            //var people = new List<string>() { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam", "Tom", "Al" };
+            //people.RemoveAt(1); //удаляется элемент по индексу
+            //people.Remove("Tom");
+            //people.RemoveRange(1, 2);
+            //people.Clear();
 
-
+            //ЗАДАНИЯ
+            //задание 1
+            //string[] predmet = ["sword", "shield", "potion", "sword", "potion", "potion"];
+            //задание 2
+            //int[] ochki = new int[] { 30, 10, 0, 12, 42 };
+            //int sum = 0;
+            //foreach (int i in ochki)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+            //задание 3
+            //int[] num = new int[] { 3, 5, 21, 6, 9, 34, 12, 65, 98, 3 };
+            //int max = num[0];
+            //for (int i=0; i<num.Length; i++)
+            //{
+            //    if (num[i] > max)
+            //    {
+            //        max = num[i];
+            //    }
+            //}
+            //Console.WriteLine(max);
+            //задание 4
+            //int[] num = new int[] { 2, 54, 21, 4, 76, 324, 879, 45, 8, 2, 4, 0, 98, 4, 6, 3, 0, 6, 3 };
+            //int ch = 0;
+            //int nech = 0;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    if (num[i] % 2 == 0)
+            //    {
+            //        ch++;
+            //    }
+            //    else
+            //    {
+            //        nech++;
+            //    }
+            //}
+            //Console.WriteLine($"Кол-во четных {ch}");
+            //Console.WriteLine($"Кол-во нечетных {nech}");
+            //задание 5
+            //int[] num = new int[] { 23, 56, 34, 87, 26, 123 };
+            //int min = num[0];
+            //for (int i = 0; i<num.Length; i++)
+            //{
+            //    if (num[i] < min)
+            //    {
+            //        min=num[i];
+            //    }
+            //}
+            //Console.WriteLine(min);
+            //задание 6
+            //List<string> p=["win", "loss", "win", "win", "loss", "win", "win", "loss"];
+            //int win = 0;
+            //int loss = 0;
+            //foreach (var i in p)
+            //{
+            //    if (i == "win")
+            //    {
+            //        win++;
+            //    }
+            //    else if (i == "loss")
+            //    {
+            //        loss++;
+            //    }
+            //}
+            //Console.WriteLine($"Кол-во побед: {win}");
+            //Console.WriteLine($"Кол-во поражений: {loss}");
+            //задание 7
+            //int[] num = new int[] { 23, 56, 34, 87, 123 };
+            //int sum = 0;
+            //for (int i =0; i<num.Length; i++)
+            //{
+            //    sum+= num[i];
+            //}
+            //int sr = sum / num.Length;
+            //Console.WriteLine(sr);
+            //задание 8
+            //List<string> z = ["Меч", "Ласточка", "Бомба", "Зелье Гром", "Ласточка", "Меч", "Ласточка", "Бомба", "Зелье Гром"];
+            //for (int i=0; i<z.Count; i++)
+            //{
+            //    if (z[i] == "Ласточка")
+            //    {
+            //        z[i] = "Кошка";
+            //    }
+            //}
+            //foreach (var i in z)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            // задание 9
+            //List<string> comp = ["Garrus", "Tali", "Mordin", "Grunt", "Jack"];
+            //List<string> rol = ["Лидер команды", "Техник", "Биотик", "Солдат", "Поддержка"];
+            //for (int i=0; i<comp.Count; i++)
+            //{
+            //    Console.WriteLine($"{rol[i]}: {comp[i]}");
+            //}
 
 
 
