a72235336f86cf00f541ad2ea16cd857702d0370
Diana <kkover073@gmail.com>
2025-09-25 20:22:23 +0300

    ob

--- Program.cs
+++ Program.cs
@@ -4,8 +4,107 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers;
-            int[]
+            //int[] numbers;
+            //int[] nums = new int[4]; 
+            //int[] nums2 = new int[4] { 1, 2, 3, 5 };
+            //int[] nums3 = new int[] { 1, 2, 3, 5 };
+            //int[] nums4 = new[] { 1, 2, 3, 5 };
+            //int[] nums5 = { 1, 2, 3, 5 };
+            //int[] num6 = [1, 2, 3, 5];
+            //int[] numbers = [1, 2, 3, 5];
+            //Console.WriteLine(numbers[3]);
+            //var n = numbers[1];
+            //Console.WriteLine(n);
+            //numbers[1] = 505;
+            //Console.WriteLine(numbers[1]);
+            //int[] numbers = [1, 2, 3, 5];
+            //Console.WriteLine(numbers[6]);
+            //Console.WriteLine(numbers.Length);
+            //Console.WriteLine(numbers[numbers.Length-1]);
+            //Console.WriteLine(numbers[numbers.Length - 2]);
+            //Console.WriteLine(numbers[numbers.Length - 3]);
+            //Console.WriteLine(numbers[^1]);
+            //Console.WriteLine(numbers[^2]);
+            //Console.WriteLine(numbers[^3]);
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //for (int i =0; i<numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = numbers[i] * 2;
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //int i = 0;
+            //while (i < numbers.Length)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //    i++;
+            //}
+            //int[] nums1 = [0, 1, 2, 3, 4, 5];
+            //int[,] nums2 = { { 0, 1, 2 }, { 3, 4, 5 } };    //двухмерный массив
+            //object[] student = ["Иван", "Игорь", 4, 4.6, 'с'];
+            //student[4] = 4;
+            //dynamic[] student2= ["Иван", "Игорь", 4, 4.6, 'с'];
+            //student2[4] = 4;
+            //Console.WriteLine(student[4].GetType());
+            //Console.WriteLine(student2[4].GetType());
+            //int[] numbers = [-1, -2, -3, 4, 5, -6, 0, 43, 56, -34, 12, 45, -102, 76, -45, 96, -4, -3, 5, 76, -6, -8, 2, 0, 5, -9];
+            //int result = 0;
+            //foreach (int number in  numbers)
+            //{
+            //    if (number > 0)
+            //    {
+            //        result++;
+            //    }
+            //}
+            //Console.WriteLine($"Число элементов больше нуля: {result}");
+            //for (int i=0; i<numbers.Length; i++)
+            //{
+            //    if (numbers[i] > 0)
+            //    {
+            //        result++;
+            //    }
+            //}
+            //Console.WriteLine($"Число элементов больше нуля: {result}");
+            //int i = 0;
+            //while (i < numbers.Length)
+            //{
+            //    if (numbers[i]>0)
+            //    {
+            //        result++;
+            //    }
+            //    i++;
+            //}
+            //Console.WriteLine($"Число элементов больше нуля: {result}");
+            //int[] numbers = [-1, -2, -3, 4, 5, -6, 0, 43, 56, -34, 12, 45, -102, 76, -45, 96, -4, -3, 5, 76, -6, -8, 2, 0, 5, -9];
+            //int n = numbers.Length;
+            //int k = n / 2;
+            //int temp;
+            //for (int i = 0; i < k; i++)
+            //{
+            //    temp = numbers[i];
+            //    numbers[i] = numbers[n - i - 1];
+            //    numbers[n - i - 1] = temp;
+            //}
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine($"{i} \t");
+            //}
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
